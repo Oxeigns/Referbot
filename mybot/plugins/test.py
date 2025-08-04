415fe4030b171feb5d8b70cec78b6cf5f72f8acd
Samrat <yashsamrat10080@gmail.com>
2025-08-04 22:31:02 +0530

    Rename ping command in test plugin

--- mybot/plugins/test.py
+++ mybot/plugins/test.py
@@ -6,9 +6,9 @@ from mybot.utils.decorators import log_errors
 LOGGER = logging.getLogger(__name__)
 LOGGER.info("Plugin loaded: %s", __name__)
 
-@Client.on_message(filters.command("ping"))
+@Client.on_message(filters.command("pingtest"))
 @log_errors
-async def ping_test(_, message):
-    LOGGER.info("ping command from %s", message.from_user.id)
+async def pingtest_cmd(_, message):
+    LOGGER.info("pingtest command from %s", message.from_user.id)
     await message.reply_text("Pong! (test plugin)")
 
