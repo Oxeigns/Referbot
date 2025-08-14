43bd388cea35fbd33c52113eb20fead31e975b84
Samrat <yashsamrat10080@gmail.com>
2025-08-14 20:32:41 +0530

    Allow running bot via import

--- mybot/__init__.py
+++ mybot/__init__.py
@@ -1,1 +1,22 @@
+"""Helper entry point for running the Refer & Earn bot as a module."""
+
 from . import config
+
+
+def run() -> None:
+    """Start the bot application.
+
+    Importing :mod:`mybot` and calling :func:`run` provides a simple way to
+    execute the bot from another Python program, for example::
+
+        import mybot
+        mybot.run()
+
+    """
+
+    from .main import run as _run
+
+    _run()
+
+
+__all__ = ["run", "config"]
